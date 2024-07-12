3be28ddb025c7ce1d3dde5d7a37771c0eee0f38d
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-12 12:05:17 +0200

    Creazione rotte

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use App\Http\Controllers\Admin\DashboardController; //<---- Import del controlle
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('/project',ProjectController::class );
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,6 +31,9 @@ Route::middleware(['auth'])
         // - il percorso "/" diventa "admin/"
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('/project', ProjectController::class );
+        Route::resource('/type',TypeController::class );
+
     });
 
 require __DIR__ . '/auth.php';
