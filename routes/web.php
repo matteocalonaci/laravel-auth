1658336b1af13514b91bd9fda974944f0254b14c
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-23 15:26:44 +0200

    Lead controller, model, migration modifiche file rotte web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\DashboardController; //<---- Import del controlle
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TechnologyController;
 use App\Http\Controllers\TypeController;
+use App\Models\Lead;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,7 @@ use App\Http\Controllers\TypeController;
 |
 */
 
-Route::resource('/project',ProjectController::class );
+Route::resource('/project', ProjectController::class);
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,10 +33,30 @@ Route::middleware(['auth'])
         // - il percorso "/" diventa "admin/"
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-        Route::resource('/project', ProjectController::class );
-        Route::resource('/type',TypeController::class );
-        Route::resource('/technology',TechnologyController::class );
+        Route::resource('/project', ProjectController::class);
+        Route::resource('/type', TypeController::class);
+        Route::resource('/technology', TechnologyController::class);
 
+        //DA COMMENTARE ALLA FINE
+        Route::get('/mailable', function () {
+            // $lead =
+            //     [
+            //         'name' => 'Matteo Calonaci',
+            //         'email' => 'matteo.calonaci@example.com',
+            //         'message' => 'Buongiorno,
+            //                     Sono Calonaci della prenotazione non rimborsabile 686/2024 in arrivo l’11/08.
+            //                     Una sesta e forse una settima persona sarebbero interessate ad unirsi alla
+            //                     vacanza.
+            //                     È possibile aggiungerle alla prenotazione pagando chiaramente il supplemento?
+            //                     Oppure riesce gentilmente a consigliare un’alternativa per chi si aggiunge?
+
+            //                     Perdoni il disturbo,
+            //                     Cordiali saluti '
+            //     ];
+            $lead = Lead::first();
+            return new App\Mail\NewContact($lead);
+        });
+        //-----------------//
     });
 
 require __DIR__ . '/auth.php';
